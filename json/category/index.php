4b877fdb7c0bebbdfd1f333e4b3f38f86fc65c42
Pete Butler <pbutler@basdesign.com>
2011-11-04 18:05:05 -0700

    hmm.. do we need <?php instead of <?

--- json/category/index.php
+++ json/category/index.php
@@ -8,11 +8,11 @@ header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
    "type" : "site_activity",
    "label" : "Site Activity",
    "data" : {
-     "www.basdesign.com" : [<? echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1202],
-     "sweetmidget.com" :   [<? echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1105],
-     "svnanaimo.com" :     [<? echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1248],
-     "dev.basdesign.com" : [<? echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1203],
-     "pete.basdesign.com" :[<? echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1147]
+     "www.basdesign.com" : [<?php echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1202],
+     "sweetmidget.com" :   [<?php echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1105],
+     "svnanaimo.com" :     [<?php echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1248],
+     "dev.basdesign.com" : [<?php echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1203],
+     "pete.basdesign.com" :[<?php echo rand(0,2000) ?>, <? echo rand(0,2000) ?>, 1147]
    },
    "key" : {
      "url" : ["last hour", "24hr avg", "last update time"]
