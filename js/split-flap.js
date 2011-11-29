37dff0f0f8789202b972409f60fe349dbb447e52
Pete Butler <pbutler@basdesign.com>
2011-11-29 15:52:49 -0800

    updated to jquery 1.7.1 (had to remove comments in json - don't validate) moved css to separate file status indicators working now.

--- js/split-flap.js
+++ js/split-flap.js
@@ -4,25 +4,8 @@ var sf = {};
 // Namespace for objects defined and used locally in templates
 sf.local = {};
 
-// ############################################################################
-// HOUSEKEEPING
-
-// Methods for getting query parameters out of the URL
-$.extend({
-  getUrlParams: function(){
-    var vars = [], 
-        params = window.location.href.slice(window.location.href.indexOf('?') + 1, window.location.href.indexOf('#')).split('&');
-    for(var i = 0; i < params.length; i++) {
-      var param = params[i].split('=');
-      vars.push(param[0]);
-      vars[param[0]] = param[1];
-    }
-    return vars;
-  },
-  getUrlParam: function(name){
-    return $.getUrlParams()[name];
-  }
-});
+/* ********************************************************************* */
+/* HOUSEKEEPING                                                          */
 
 Array.prototype.rotate = (function() {
   var unshift = Array.prototype.unshift,
@@ -35,18 +18,101 @@ Array.prototype.rotate = (function() {
   };
 })();
 
-// ############################################################################
-// DATA VISUALIZATION
 sf.chart = {
+
+  /* ********************************************************************* */
+  /* DATA RETRIEVAL AND FORMATTING                                         */
+
+  init: function() {
+    sf.chart.splitFlap.init();
+  },
+
+  dataBaseUrl: "", // change this if you want to get data from somewhere else
   
   dataUrl: function() {
-    // Note: sf.chart.dataBaseUrl set up in template 
     var params = arguments[0],
         chartStyle = arguments[1],
         dataPath = "json/category";
     return sf.chart.dataBaseUrl + dataPath + "/index.php?" + params;  // NOTE TEMPORARY CHANGE HERE
   },
   
+  formatData: function() {
+    // The "data" node in this map uses category names as keys. Because there 
+    // is no impled order in this data structure, we need to output array of 
+    // the following format, thus:
+    // Input:  data: {
+    //                 "category1":[value1,value2,value3], 
+    //                 "category2":[value4,value5,value6], 
+    //                 "category3":[value7,value8,value9]
+    //               }
+    // Output: [
+    //           {"label":"category1","data":[value1,value2,value3]},
+    //           {"label":"category2","data":[value4,value5,value6]},
+    //           {"label":"category3","data":[value7,value8,value9]}
+    //         ]
+    //
+    // NOTE: Only non-zero values are included (so a set of all zero values will return
+    //       an empty array.
+    //       
+    // NOTE: 'asc' and 'desc' sort based on the first value in the data array
+    // 
+    var input = arguments[0],
+        format = arguments[1],
+        options = arguments[2],
+        series = input[0].data,
+        i=0, j=0, k=0, index=0, value=0,
+        output = [],
+        truncatedOutput = [],
+        other = [];
+    // first pass: create new data structure
+    for(index in series) {
+      if(series.hasOwnProperty(index)) {
+        // add this category to the output (unless its in the "ignore" array)
+        if(jQuery.inArray(index, options.ignore) === -1) {
+          var data = series[index];
+          output.push( {"label" : index, "data" : data } );
+          i++;
+        }
+      }
+    }
+    // pick up any sorting options
+    if(options.sort === "desc") {
+      output.sort(function(a,b){
+        return a.data[0]>b.data[0]?-1:1;
+      });
+    } else if(options.sort === "asc") {
+      output.sort(function(a,b){
+        return a.data[0]>b.data[0]?1:-1;
+      });
+    }
+    // second pass: truncate/maxResults
+    for(i=0;i < output.length; i++){
+      if(i < options.truncate) {
+        truncatedOutput.push(output[i]);
+      } else {
+        for(j=0;j<output[i].data.length;j++){
+          // 'other[]' will be empty on the first trip
+          // through this loop, so give it
+          // zero values out to output[i].data.length
+          if(typeof(other[j]) === 'number') {
+            other[j] += parseInt(output[i].data[j],10);
+          } else {
+            other[j] = 0;
+          }
+        }
+      }
+      // bail if we hit maxResults
+      if(i === parseInt(options.maxResults,10)) {
+        break;
+      }
+    }
+    // populate "Other" node
+    if(other.length > 0) {
+      truncatedOutput.push( {"label" : "other", "data" : other, "color": "#EFEFEF" } );
+    }
+    return truncatedOutput;
+  },
+
   render: function() {
     var container = arguments[0]; // expects the container object
     var chartStyle = container.find("input[name=chartStyle]").val();
@@ -62,7 +128,6 @@ sf.chart = {
       "ignore": ignores
     };
     var params = container.find(".chartPrefs input").serialize();
-
     $.getJSON(
       sf.chart.dataUrl(params, chartStyle),
       function(data) {
@@ -79,6 +144,13 @@ sf.chart = {
     );
 
   },
+
+  /* END DATA RETRIEVAL AND FORMATTING                                     */
+  /* ********************************************************************* */
+
+
+  /* ********************************************************************* */
+  /* SPLIT FLAP                                                            */
 
   splitFlap: {
   
@@ -165,22 +237,26 @@ sf.chart = {
           elements = target.find(".character, .number, .full, .logo"),
           strLen = elements.size(),
           i, characters;
-      // split flap displays are only uppercase
-      input = input.toUpperCase();
-      // get individual characters
-      characters = input.split("");
-      // pad the array with spaces (to clear any existing characters)
-      for(i=0;i<strLen;i++) {
-        if(typeof(characters[i]) === 'undefined') {
-          characters[i] = " ";
+      if(target.hasClass("status")) { // status renders differently, just add the class "on" to the correct element
+        target.find("div").removeClass("on");
+        target.find(".s"+input).addClass("on");
+      } else {  // otherwise, this group is composed of split-flap elements
+        input = input.toUpperCase();
+        // get individual characters and pad the array 
+        // with spaces (to clear any existing characters)
+        characters = input.split("");
+        for(i=0;i<strLen;i++) {
+          if(typeof(characters[i]) === 'undefined') {
+            characters[i] = " ";
+          }
         }
-      }
-      // trim the array to the number of display elements
-      characters = characters.slice(0,strLen);
-      // assign them to the display elements
-      for(var i=0;i<characters.length;i++) {
-        // TODO: is there a more efficient way to do this?
-        sf.chart.splitFlap.change($(elements[i]).find("span"), characters[i]);
+        // trim the array to the number of display elements
+        characters = characters.slice(0,strLen);
+        // assign them to the display elements
+        for(var i=0;i<characters.length;i++) {
+          // TODO: is there a more efficient way to do this?
+          sf.chart.splitFlap.change($(elements[i]).find("span"), characters[i]);
+        }
       }
     },
     
@@ -198,8 +274,6 @@ sf.chart = {
       }
       // rotate the dom element's stored array to the new order for next time
       container.data("order", values.rotate(index));
-      // change the number in the markup
-      // container.text(c);
     },
     
     show: function() {
@@ -225,91 +299,17 @@ sf.chart = {
       }).fadeIn(50);
     }
   
-  },
-
-  formatData: function() {
-    // The "data" node in this map uses category names as keys. Because there 
-    // is no impled order in this data structure, we need to output array of 
-    // the following format, thus:
-    // Input:  data: {
-    //                 "category1":[value1,value2,value3], 
-    //                 "category2":[value4,value5,value6], 
-    //                 "category3":[value7,value8,value9]
-    //               }
-    // Output: [
-    //           {"label":"category1","data":[value1,value2,value3]},
-    //           {"label":"category2","data":[value4,value5,value6]},
-    //           {"label":"category3","data":[value7,value8,value9]}
-    //         ]
-    //
-    // NOTE: Only non-zero values are included (so a set of all zero values will return
-    //       an empty array.
-    // NOTE: 'asc' and 'desc' sort based on the first value in the data array
-    var input = arguments[0],
-        format = arguments[1],
-        options = arguments[2],
-        series = input[0].data,
-        i=0, j=0, k=0, index=0, value=0,
-        output = [],
-        truncatedOutput = [],
-        other = [];
-    // first pass: create new data structure
-    for(index in series) {
-      if(series.hasOwnProperty(index)) {
-        // add this category to the output (unless its in the "ignore" array)
-        if(jQuery.inArray(index, options.ignore) === -1) {
-          var data = series[index];
-          output.push( {"label" : index, "data" : data } );
-          i++;
-        }
-      }
-    }
-    // pick up any sorting options
-    if(options.sort === "desc") {
-      output.sort(function(a,b){
-        return a.data[0]>b.data[0]?-1:1;
-      });
-    } else if(options.sort === "asc") {
-      output.sort(function(a,b){
-        return a.data[0]>b.data[0]?1:-1;
-      });
-    }
-    // second pass: truncate/maxResults
-    for(i=0;i < output.length; i++){
-      if(i < options.truncate) {
-        truncatedOutput.push(output[i]);
-      } else {
-        for(j=0;j<output[i].data.length;j++){
-          // 'other[]' will be empty on the first trip
-          // through this loop, so give it
-          // zero values out to output[i].data.length
-          if(typeof(other[j]) === 'number') {
-            other[j] += parseInt(output[i].data[j],10);
-          } else {
-            other[j] = 0;
-          }
-        }
-      }
-      // bail if we hit maxResults
-      if(i === parseInt(options.maxResults,10)) {
-        break;
-      }
-    }
-    // populate "Other" node
-    if(other.length > 0) {
-      truncatedOutput.push( {"label" : "other", "data" : other, "color": "#EFEFEF" } );
-    }
-      
-    return truncatedOutput;
   }
+  /* END SPLIT FLAP                                                        */
+  /* ********************************************************************* */
   
 };
 
 
-// ############################################################################
-// DOCUMENT LOAD
+/* ********************************************************************* */
+/* DOCUMENT LOAD                                                         */
 
 $(document).ready(function() {
-  sf.chart.splitFlap.init();
+  sf.chart.init();
 });
 
--- json/category/index.php
+++ json/category/index.php
@@ -2,54 +2,26 @@
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 ?>
-// Activity tracker JSON example.
-/*
-[
-  {
-   "type" : "site_activity",
-   "label" : "Site Activity",
-   "data" : {
-     "www.basdesign.com" : [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1202],
-     "sweetmidget.com" :   [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1105],
-     "svnanaimo.com" :     [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1248],
-     "dev.basdesign.com" : [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1203],
-     "pete.basdesign.com" :[<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1147],
-     "www.google.com" : [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1202],
-     "fark.com" :   [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1105],
-     "wikipedia.org" :     [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1248],
-     "youtube.com" : [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1203],
-     "vimeo.com" :[<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1147],
-     "amazon.com" : [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1202],
-     "twitter.com" :   [<?php echo rand(0,2000) ?>, <?php echo rand(0,2000) ?>, 1105]
-   },
-   "key" : {
-     "url" : ["last hour", "24hr avg", "last update time"]
-   }
-  }
-]
-*/
-
-// Airport Solari Board Example.
 [
   {
    "type" : "departures",
    "label" : "Departures",
    "data" : {
-     " 101" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "London", "0", "C21", "1244", ""],
-     "  96" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Madrid", "1", "A7", "1244", ""],
-     "7420" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Belgrade", "2", "A18", "1244", ""],
-     "   7" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Moscow", "3", "D44", "1244", ""],
-     "  99" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "London", "4", "A12", "1244", ""],
-     " 215" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Geneve", "5", "B2", "1244", ""],
-     " 174" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Athens", "6", "B14", "1244", ""],
-     "  41" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Munich", "7", "C6", "1244", ""],
-     "4476" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Dubai", "8", "D16", "1244", ""],
-     " 112" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Madrid", "8", "A6", "1244", ""],
-     "2145" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "New York", "9", "C7", "1244", ""],
-     "7772" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Los Angeles", "3", "B12", "1244", ""],
+     " 101" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "London", "0", "C21", "1", "est 2130+"],
+     "  96" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Madrid", "1", "A7", "0", ""],
+     "7420" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Belgrade", "2", "A18", "0", ""],
+     "   7" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Moscow", "3", "D44", "0", ""],
+     "  99" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "London", "4", "A12", "1", "cancelled"],
+     " 215" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Geneve", "5", "B2", "0", ""],
+     " 174" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Athens", "6", "B14", "0", ""],
+     "  41" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Munich", "7", "C6", "0", ""],
+     "4476" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Dubai", "8", "D16", "0", ""],
+     " 112" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Madrid", "8", "A6", "0", ""],
+     "2145" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "New York", "9", "C7", "0", ""],
+     "7772" : ["<?php echo sprintf("%02d", rand(0,23)) ?><?php echo sprintf("%02d", rand(0,59)) ?>", "Los Angeles", "3", "B12", "0", ""]
    },
    "key" : {
-     "flight" : ["scheduled", "city", "airline", "gate", "actual", "remarks"]
+     "flight" : ["scheduled", "city", "airline", "gate", "status", "remarks"]
    }
   }
 ]
