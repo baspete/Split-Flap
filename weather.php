610b7e7c22a67e5bc5bd658bcb59afbe5dbd984f
Pete Butler <pbutler@basdesign.com>
2011-12-08 16:09:30 -0800

    Simplified Collection->Model->View. Also, since the most obvious thing to do here is to use the Collection to handle sorting, truncating, etc., removed all of sf.data from split-flap.js and called the new file split-flap-mvc.js. Split-flap.js might be an evolutionary dead-end, but let's keep it around for now.

--- weather.php
+++ weather.php
@@ -20,6 +20,9 @@
   
         <!-- Header: 30px/char, 15px/separator, 120px/logo -->
         <div class="header" style="width:120px;margin-left:0px;">Station</div>
+        <div class="header" style="width:90px;margin-left:30px;">Temp</div>
+        <div class="header" style="width:150px;margin-left:30px;">Wind Direction</div>
+        <div class="header" style="width:120px;margin-left:30px;">Wind Speed</div>
 
         <!-- rows will be placed here dynamically from #row_template -->
         
@@ -32,7 +35,7 @@
     <script type="text/javascript" src="js/jquery-1.7.1-min.js"></script>
     <script type="text/javascript" src="js/underscore.js"></script>
     <script type="text/javascript" src="js/backbone.js"></script>
-    <script type="text/javascript" src="js/split-flap.js"></script>
+    <script type="text/javascript" src="js/split-flap-mvc.js"></script>
 
     <!-- ============================================ -->
     <!-- ROW TEMPLATE                                 -->
@@ -67,22 +70,43 @@
 
     <script type="text/javascript">
 
+      // This Collection holds the station Models. 
+      // It tells its models to fetch() their data at set intervals. 
       var Stations = Backbone.Collection.extend({
         initialize: function(){
-          this.bind("change",function(eventName){
-            console.log("collection changed: ",eventName)
-          },this);
-        }
+          var stations = this.models; // need access inside functions
+
+          // This function fetches models sequentially, 
+          // much the way a real Solari board controller works.
+          var fetchModels = function(models) {
+            var i=0;
+            var loop = function() {
+              setTimeout(function () {
+                models[i].fetch({dataType:sf.plugins.wunderground.dataType});
+                i++;
+                if (i < models.length) {
+                  loop(i); 
+                }
+               }, 500); // sequence interval
+            };
+            loop();
+          };
+
+          // Fetch the models and repeat at intervals
+          fetchModels(stations);
+          setInterval(function(){
+            fetchModels(stations);
+          }, 5000); // refresh inteval
+
+        } // end initialize
       });
 
+      // Here's the Model for an individual station. It has
+      // one method, "normalize()", which returns an object 
+      // which can be passed directly to sf.display.loadRow()
+      // TODO: perhaps this should be handled through parse()
+      //       so we can just pass the whole model?
       var Station = Backbone.Model.extend({
-        initialize: function(){
-          var station = this;
-          setInterval(function(){
-            console.log("fetching model ",station.id)
-            station.fetch();
-          }, 5000); // refresh inteval
-        },
         normalize: function(){
           var observation = this.get("current_observation");
           var normalizedData = {}
@@ -98,17 +122,21 @@
         }
       });
 
+      // Here's the View for an individual station. When it detects a change
+      // in its model it re-renders itself into its row by calling sf.display.loadRow().
       var StationView = Backbone.View.extend({
         template: _.template($('#row_template').html()),
         initialize: function(){
+          // create the row markup and insert it into the container.
           $("#chart1").append(this.template({"id":this.model.id})); // could send the whole model, but why?
           this.el = $("#"+this.model.id);
+          // initialize sf.display for this row
           sf.display.initRow(this.el);
+          // anytime this model changes, re-render this row
           this.model.bind("change", this.render, this);
         },
         render: function(){
-          // console.log("rendering view: ",this.model.id, "with data: ",this.model.normalize() );
-          sf.display.loadRow(this.model.normalize(),$("#"+this.model.id));
+          sf.display.loadRow(this.model.normalize(),this.el);
         }
       });
 
@@ -119,28 +147,20 @@
         $.ajax({
           url: sf.plugins.wunderground.stationsUrl("<?php echo $_GET["data"] ?>"),
           // dataType: 'jsonp',
-          dataType: 'json',
+          dataType: sf.plugins.wunderground.dataType,
           success: function(response){
-            var codes = sf.plugins.wunderground.formatStationsData(response);
-            var l = codes.length;
-            var s = []; // a temporary array to hold models - we'll create the collection when this is full
-            var i = 0;
-            for (i=0;i<l;i++){
-              var station = new Station({id:codes[i]});
-              station.url = sf.plugins.wunderground.stationUrl(codes[i]);
-              station.fetch({
-                // dataType: 'jsonp',
-                dataType: 'json',
-                success: function(response){
-                  s.push(response.toJSON());
-                  // if we've got all the stations back, create the collection
-                  if(s.length === l) {
-                    var stations = new Stations(s);
-                  }
-                }
-              });
-              // finally, create a view for this station
-              var stationView = new StationView({model:station});
+            var stations = new Stations, // Create a Collection
+                codes = sf.plugins.wunderground.formatStationsData(response); // Format the response to get just the array of station codes
+            for (var i=0;i<codes.length;i++){
+              var station = new Station({id:codes[i]}); // Create a Model for this station
+              var stationView = new StationView({model:station}); // Create a view for this station
+              station.url = sf.plugins.wunderground.stationUrl(codes[i]); // Set the data url for this station
+              stations.add(station); // Add the station to the Collection
+            }
+          },
+          statusCode: {
+            404: function() {
+              $("#chart1").html("<h1>Station <?php echo $_GET["data"] ?> Not Found</h1>")
             }
           }
         });        
