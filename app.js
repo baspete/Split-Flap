91b3e093a6b049c6ab0c1e4d8dee15d7d7abbc6d
Pete Butler <pete.butler@ge.com>
2018-12-21 15:38:13 -0500

    moving to node from PHP

---
+++ app.js
@@ -0,0 +1,127 @@
+/* global require console process Promise module */
+
+const express = require('express'),
+  app = express();
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomAirline() {
+  const airlines = [
+    'AFL',
+    'AAL',
+    'BAW',
+    'DAL',
+    'UAE',
+    'KLM',
+    'DLH',
+    'RYR',
+    'UAL',
+    'AWE'
+  ];
+  return airlines[getRandomInt(airlines.length - 1)];
+}
+
+function getRandomTime() {
+  return '01:23';
+}
+
+function getRandomFlight() {
+  return getRandomInt(2000);
+}
+
+function getRandomGate() {
+  const t = ['A', 'B', 'C'][getRandomInt(2)];
+  const g = getRandomInt(30);
+  return `${t}${g}`;
+}
+
+function getRandomCity() {
+  const c = [
+    'Atlanta',
+    'Baltimore',
+    'Charleston',
+    'Durban',
+    'Edinburgh',
+    'Frankfurt',
+    'Galveston',
+    'Havana',
+    'Iowa City',
+    'Jakarta',
+    'Karachi',
+    'Los Angeles',
+    'Mexico City',
+    'Nairobi',
+    'Ontario',
+    'Pittsburgh',
+    'Quebec City',
+    'Roanoake',
+    'San Diego',
+    'Tallahassee'
+  ];
+  return c[getRandomInt(6)];
+}
+
+function getRandomTime() {
+  let hrs = getRandomInt(23).toString();
+  let mins = getRandomInt(59).toString();
+  for (let i = hrs.length; i < 2; i++) {
+    hrs = '0' + hrs;
+  }
+  for (let i = mins.length; i < 2; i++) {
+    mins = '0' + mins;
+  }
+  return `${hrs}${mins}`;
+}
+
+function getRandomMessage() {
+  let m = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    `${getRandomInt(30)}m early`,
+    `delayed`
+  ];
+  return m[getRandomInt(10)];
+}
+
+// ========================================================================
+// API
+
+app.use('/api/flights', (req, res) => {
+  let r = {
+    data: []
+  };
+
+  for (let i = 0; i < 12; i++) {
+    let msg = getRandomMessage();
+    let status = msg === 'delayed' ? 1 : 0;
+    r.data.push({
+      airline: getRandomAirline(),
+      flight: getRandomFlight(),
+      city: getRandomCity(),
+      gate: getRandomGate(),
+      scheduled: getRandomTime(),
+      remarks: msg,
+      status: status
+    });
+  }
+
+  res.json(r);
+});
+
+// ========================================================================
+// WEB APP
+app.use('/', express.static('public'));
+
+// ========================================================================
+// WEB SERVER
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log('split flap started on port ' + port);
---
+++ public/plugins/airport/custom.js
@@ -0,0 +1,30 @@
+sf.display.ImageDrum = function() {
+  this.order = [
+    ' ',
+    'AFL',
+    'AAL',
+    'BAW',
+    'DAL',
+    'UAE',
+    'KLM',
+    'DLH',
+    'RYR',
+    'UAL',
+    'AWE'
+  ];
+};
+
+sf.plugins.airport = {
+  dataType: 'json',
+
+  // input: map of parameters
+  // output: url
+  url: function(options) {
+    var base_url = 'api/flights';
+    return base_url + '?data=' + options.data;
+  },
+
+  formatData: function(response) {
+    return response.data;
+  }
+};
